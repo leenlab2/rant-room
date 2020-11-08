2f88f839122944064331d697ba45d49a81b5c522
leenlab2 <74102544+leenlab2@users.noreply.github.com>
2020-11-08 19:53:23 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # main.py
 from flask import Flask
-from flask import url_for, render_template, request
+from flask import url_for, render_template, request, send_from_directory
 from test_database import insert_user_message
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/template/<path:path>')
+def send_tp(path):
+    return send_from_directory('template', path)
 
 
 @app.route('/')
@@ -19,11 +24,13 @@ def foo():
     insert_user_message(thoughts)
     return render_template('index.html')
 
+
 @app.route("/test", methods=['GET'])
 def test():
     cursor.execute("select user_message from messages")
     result = cursor.fetchall()
     return render_template("Board.html", data=result)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
