103f3da8e6fb999b22fe2bdfb22a7dd00b6d4da0
leenlab2 <74102544+leenlab2@users.noreply.github.com>
2020-11-08 17:19:49 +0400

    Add files via upload

---
+++ static/fun_rooms.js
@@ -0,0 +1,9 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("menuSidebar").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("menuSidebar").style.width = "0";
+}
